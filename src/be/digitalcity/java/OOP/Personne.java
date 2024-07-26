f4598c1482e27acf79e7bc660edc6b8286775b3e
nel <nawfal.elkhaznagi@gmail.com>
2024-07-26 15:50:47 +0200

    finished classes and oop

--- src/be/digitalcity/java/OOP/Personne.java
+++ src/be/digitalcity/java/OOP/Personne.java
@@ -6,24 +6,26 @@ public class Personne {
     String prenom;
     String nom;
 
-    void appeler(Object personne) {
-        System.out.println("Bonjour " + personne.prenom + personne.nom " , je me nomme " + this.prenom + " " + this.nom );
+    void appeler(Personne aSaluer) {
+        System.out.printf("Bonjour %s %s, je m'appelle %s %s \n", aSaluer.prenom, aSaluer.nom, this.prenom, this.nom);
     }
 
+    ;
 
-    public Personne (String prenom, String nom)
-    {
+
+    public Personne(String prenom, String nom) {
         this.prenom = prenom;
         this.nom = nom;
     }
 
 
-
     public static void main(String[] args) {
 
-        Personne Nawfal = new Personne();
+        Personne nawfal = new Personne("Nawfal", "Elkhaznagi");
+        Personne john = new Personne("John", "Doe");
 
-
+        nawfal.appeler(john);
+        john.appeler(nawfal);
 
 
     }
